e0d48bb8f629f2d8e24802fcb5d436945f7b46c5
Humberto Esparza Ramos <humberto.dev96@gmail.com>
2018-02-15 16:32:10 -0600

    Modifying wait_notify almost 100% finished

--- Kotlin/wait_notify.kt
+++ Kotlin/wait_notify.kt
@@ -1,21 +1,66 @@
-private val lock = java.lang.Object()
+import java.util.Random
+//import kotlin.concurrent.thread
 
-fun produce() = synchronized(lock) {  
-  while (items >= maxItems) {
-    lock.wait()
+data class Cuenta(var saldo: Int)
+
+fun getRandom(max: Int) : Int = Random().nextInt(max)+1
+
+class Operaciones(private val cuenta: Cuenta) : Thread() {
+
+  @Volatile private var saldo = 2
+  private val rand = Random()
+
+  private val lock = java.lang.Object()
+
+  override fun run() {
+    for ( i in 0..14 ) {
+      when( getRandom(2) ) {
+        1 -> produce()
+        2 -> consume()
+      }
+    }
   }
-  Thread.sleep(rand.nextInt(100).toLong())
-  items++
-  println("Produced, count is $items: ${Thread.currentThread()}")
-  lock.notifyAll()
+
+  fun produce() = synchronized(lock) {
+    while (saldo >= cuenta.saldo) {
+      println("Waiting Produce ${Thread.currentThread()}")
+      lock.wait()
+    }
+    Thread.sleep(200)
+    //if ( saldo+2 < cuenta.saldo)  
+      saldo += 2
+    //else
+    //  saldo += 1
+    
+    println("Produced, count is $saldo")
+    lock.notifyAll()
+  }
+
+  fun consume() = synchronized(lock) {
+    while (saldo <= 0) {
+      println("Waiting Consume ${Thread.currentThread()} --- Saldo = ${saldo}")
+      lock.wait()
+    }
+    Thread.sleep(200)
+    saldo--
+    println("Consumed, count is $saldo")
+    //lock.notifyAll()
+  }
 }
 
-fun consume() = synchronized(lock) {  
-  while (items <= 0) {
-    lock.wait()
+fun main(args: Array<String>) {
+  println("Starting...")
+
+  val cuenta: Cuenta = Cuenta(5)
+
+  val operaciones: Array<Operaciones> = arrayOf( Operaciones(cuenta), 
+                                                 Operaciones(cuenta), 
+                                                 Operaciones(cuenta))
+
+  for (j in 0..2) {
+    operaciones[j].start()
   }
-  Thread.sleep(rand.nextInt(100).toLong())
-  items--
-  println("Consumed, count is $items: ${Thread.currentThread()}")
-  lock.notifyAll()
+
+  //Thread.sleep(1000)
+  //println("...finishing")
 }
