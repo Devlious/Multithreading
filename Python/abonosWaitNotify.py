808b3ba59095386ab3aa830c38fe59e8d20cb2cd
Devlious <humberto.dev96@gmail.com>
2018-03-08 13:44:04 -0600

    Adding Abonos-WaitNotify

--- Python/abonosWaitNotify.py
+++ Python/abonosWaitNotify.py
@@ -2,8 +2,10 @@ import threading
 import time
 import random
 
+abono = True 
+
 class BankAccount(threading.Thread):
-    acctBalance = 50
+    acctBalance = 10
 
     def __init__(self, name):
         threading.Thread.__init__(self)
@@ -19,10 +21,13 @@ class BankAccount(threading.Thread):
                 BankAccount.get_money(self)
 
     def get_money(self):
+        global abono
+
         money_request = random.randrange(100)+1
 
         condition.acquire()
-        while BankAccount.acctBalance - money_request < 0:
+
+        while BankAccount.acctBalance - money_request < 0 and abono == False:
             print("{} waiting for money [${}]".format(self.name, money_request))
             condition.wait()
 
@@ -31,18 +36,24 @@ class BankAccount(threading.Thread):
         condition.release()
 
         BankAccount.acctBalance -= money_request
+        if BankAccount.acctBalance < 0:
+            abono = False
         print("New account balance is : ${}".format(BankAccount.acctBalance))
 
         print("\n")
         time.sleep(0.5)
 
     def set_money(self):
+        global abono
         money_request = random.randrange(100)+1
         condition.acquire()
         print("+ {} deposit : ${}".format(self.name, money_request))
 
         BankAccount.acctBalance += money_request
         print("New account balance is : ${}".format(BankAccount.acctBalance))
+        if not abono and BankAccount.acctBalance >= 0:
+            abono = True
+
         condition.notifyAll()
         condition.release()
 
@@ -55,8 +66,7 @@ class BankAccount(threading.Thread):
 threadLock = threading.Lock()
 condition = threading.Condition()
 
-threads = [BankAccount({"Beto":0}), BankAccount({"Diego":0}),
-           BankAccount({"Lolo":0})]
+threads = [BankAccount("Beto"), BankAccount("Diego"), BankAccount("Lolo")]
 
 for t in threads:
     t.start()
